54d2dc5707fa834b509b3b11e4337749ef6ac32f
Chuwei.C <122242630+Chuweic@users.noreply.github.com>
2025-11-01 00:33:38 -0700

    Delete src/soccerclub directory

--- src/soccerclub/Position.java
+++
@@ -1,9 +0,0 @@
-package soccerclub;
-
-/**
- * This is the enum class stating the position of a soccer team. A team has Goalie, Defenders,
- * Midfielders, Forward positions.
- */
-public enum Position {
-  Goalie, Defender, Midfield, Forward;
-}
